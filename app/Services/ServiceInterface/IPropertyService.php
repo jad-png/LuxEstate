70a3c8c0bf55d2d9a4e55f61c325e54db8650773
jad-png <j.belassiria@uca.ca.ma>
2025-04-02 19:17:24 +0000

    FEAT: created a crud interface that holds the crud methods where all other interfaces extend form thisinterface

---
+++ app/Services/ServiceInterface/IPropertyService.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace App\Services\ServiceInterface;
+
+use App\Models\Property;
+use App\Models\PropertyImage;
+use App\Models\PropertyVideos;
+use App\Services\Interfaces\CrudInterface;
+
+interface IPropertyService extends CrudInterface
+{        
+    public function addImage(int $propertyId, string $imagePath): PropertyImage;
+
+    public function addVideo(int $PropertyId, string $videoPath): PropertyVideos;
+
+    public function attachFeatures(int $propertyId, array $featuresIds): void;
+}
