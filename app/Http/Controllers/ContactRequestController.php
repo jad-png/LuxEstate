78664a677a6d69ca08a967730fcbc6e8b7b85d92
jad-png <j.belassiria@uca.ca.ma>
2025-04-08 15:11:38 +0100

    FEAT: implement constructor and method stubs for ContactRequestController

--- app/Http/Controllers/ContactRequestController.php
+++ app/Http/Controllers/ContactRequestController.php
@@ -7,4 +7,25 @@ use Illuminate\Http\Request;
 class ContactRequestController extends Controller
 {
     // create, resolve, getMyRequests
+    protected $contactService;
+
+    public function __construct($contactService)
+    {
+        $this->contactService = $contactService;
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function resolve()
+    {
+
+    }
+
+    public function getMyRequests()
+    {
+        
+    }
 }
