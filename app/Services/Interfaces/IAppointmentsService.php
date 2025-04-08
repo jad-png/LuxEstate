0e319f81ea279816196136a92afe9f9960acacf6
jad-png <j.belassiria@uca.ca.ma>
2025-04-08 19:05:06 +0100

    FEAT: add IAppointmentsService interface for appointment management

---
+++ app/Services/Interfaces/IAppointmentsService.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Appointment;
+use Illuminate\Database\Eloquent\Collection;
+
+interface IAppointmentsService
+{
+    /**
+     * Summary of createAppointment
+     * @param int $clientId
+     * @param int $agentId
+     * @param string $date
+     * @param string $time
+     * @return Appointment
+     */
+    public function createAppointment($clientId, $agentId, $date, $time);
+
+    /**
+     * Summary of getClientAppointments
+     * @param int $clientId
+     * @return Collection
+     */
+    public function getClientAppointments($clientId);
+}
