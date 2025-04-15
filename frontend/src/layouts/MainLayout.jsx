34b166cac9e6c5400dda301abac988a87064008b
jad-png <j.belassiria@uca.ca.ma>
2025-04-15 15:03:19 +0100

    FEAT: add Header and Footer components and integrate them into MainLayout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,20 @@
 import { Outlet } from 'react-router';
+import Header from '../components/Global/Header';
+import Footer from '../components/Global/Footer';
 
 const MainLayout = () => {
-    return 
-}
+    return (
+        <div>    
+            <Header />
+
+            <br/>
+            <main>
+                <Outler/>
+            </main>
+        
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;
