4ffa70c2dc8f5981e485410327c87f3aef787c57
jad-png <j.belassiria@uca.ca.ma>
2025-04-23 15:20:25 +0100

    FEAT: add AddCategoryRequest and UpdateCategoryRequest for category validation, and implement CategoryService for CRUD operations

---
+++ backend/app/Services/CategoryService.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace App\Services;
+
+use App\Http\Requests\AddCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+use App\Services\Interfaces\CrudInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class CategoryService implements CrudInterface
+{
+    /**
+     * Summary of all
+     * @return Collection
+     */
+    public function all() {
+        return Category::all();
+    }
+
+    /**
+     * Summary of find
+     * @param int $id
+     * @return Category
+     */
+    public function find($id) {
+        return Category::find($id);
+    }
+
+    
+    /**
+     * Summary of create
+     * @param AddCategoryRequest $request
+     * @return Category
+     */
+    public function create($request) {
+        return Category::create($request->validated());
+    }
+    
+    /**
+     * Summary of update
+     * @param int $id
+     * @param UpdateCategoryRequest $request
+     * @return Category
+     */
+    public function update(int $id, $request) {
+        $category = Category::find($id);
+        $category->update($request->validated());
+        return $category;
+    }   
+
+}
