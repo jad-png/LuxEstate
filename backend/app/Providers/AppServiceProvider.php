2c8f85d4cb5f4d2076ef74605b14208c5c423796
jad-png <j.belassiria@uca.ca.ma>
2025-04-25 17:31:21 +0100

    FEAT: bind IBlogCategoryService to BlogCategoryService in AppServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ use App\Services\Interfaces\IContactRequestService;
 use App\Services\Interfaces\IFeaturesService;
 use App\Services\Interfaces\IPropertyService;
 use App\Services\AppointmentsService;
+use App\Services\BlogCategoryService;
 use App\Services\BlogService;
+use App\Services\Interfaces\IBlogCategoryService;
 use App\Services\Interfaces\IBlogService;
 use App\Services\Interfaces\INotificationService;
 use App\Services\NotificationService;
@@ -32,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IAppointmentsService::class, AppointmentsService::class);
         $this->app->bind(IBlogService::class, BlogService::class);
         $this->app->bind(INotificationService::class, NotificationService::class);
+        $this->app->bind(IBlogCategoryService::class, BlogCategoryService::class);
     }
 
     /**
