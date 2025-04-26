2eb0da5ede8bd95836a4407e9debc0e31d5d5094
jad-png <j.belassiria@uca.ca.ma>
2025-04-26 22:17:55 +0100

    FEAT: update post store to support pagination in fetchAllPosts and fetchPostsByCategory

--- frontend/src/stores/storePost.jsx
+++ frontend/src/stores/storePost.jsx
@@ -2,14 +2,14 @@ import { create } from "zustand";
 import api from "../services/api";
 
 const useStorePost = create((set) => ({
-  posts: [],
+  posts: { data: [], current_page: 1, last_page: 1, total: 0 },
   error: null,
   loading: false,
 
-  fetchAllPosts: async () => {
+  fetchAllPosts: async (page = 1) => {
     set({ loading: true, error: null });
     try {
-      const response = await api.get("blog/posts");
+      const response = await api.get(`blog/posts?page=${page}`);
       const apiResponse = response.data.data;
       console.log(response);
 
@@ -22,10 +22,10 @@ const useStorePost = create((set) => ({
     }
   },
 
-  fetchPostsByCategory: async (categoryId) => {
+  fetchPostsByCategory: async (categoryId, page = 1) => {
     set({ loading: true, erorr: null });
     try {
-      const response = await api.get(`blog/posts/category/${categoryId}`);
+      const response = await api.get(`blog/posts/category/${categoryId}?page=${page}`);
       const apiResponse = response.data.data;
       set({ posts: apiResponse, loading: false, error: null });
     } catch (error) {
