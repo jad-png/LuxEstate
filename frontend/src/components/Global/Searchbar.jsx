77ad8a8074e217dfff98a2eaaa98d4a3ab23947b
jad-png <j.belassiria@uca.ca.ma>
2025-05-04 16:50:21 +0100

    FEAT: enhance SearchBar component with state management and dropdown functionality

--- frontend/src/components/Global/Searchbar.jsx
+++ frontend/src/components/Global/Searchbar.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router";
 
-export function SearchBar()
-{
-    return (
-        <div className="flex items-center space-x-2 w-2/3">
-          <input
-            type="text"
-            placeholder="Search for anything..."
-            className="w-full max-w-md p-2 border border-[#e5e5e5] text-[#666666] manrope focus:outline-none focus:border-[#a27d56]"
-          />
-          <button
-            className="px-4 py-2 bg-[#a27d56] text-white manrope hover:bg-[#8b6a47]"
-          >
-            Search
-          </button>
-        </div>
-    );
+export function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const navigate = useNavigate();
+  return (
+    <div className="flex items-center space-x-2 w-2/3">
+      <input
+        type="text"
+        placeholder="Search for anything..."
+        className="w-full max-w-md p-2 border border-[#e5e5e5] text-[#666666] manrope focus:outline-none focus:border-[#a27d56]"
+      />
+      <button className="px-4 py-2 bg-[#a27d56] text-white manrope hover:bg-[#8b6a47]">
+        Search
+      </button>
+    </div>
+  );
 }
