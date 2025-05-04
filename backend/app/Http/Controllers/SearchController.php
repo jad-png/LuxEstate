1b9c5ba2dd6cb230ed55b3b32198ea4d5345ed6a
jad-png <j.belassiria@uca.ca.ma>
2025-05-04 16:33:54 +0100

    FEAT: implement search functionality using ISearchService in SearchController

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Interfaces\ISearchService;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    protected $searchService;
+
+    public function __construct(ISearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+
+    public function search (Request $request)
+    {
+        $search = $this->searchService->search($request);
+        
+        return response()->json($search); 
+    }
 }
